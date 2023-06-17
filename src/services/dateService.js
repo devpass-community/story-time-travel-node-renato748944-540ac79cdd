00f9e5a00850a21d13649e9881af797de4197d90
renatonunes74 <renato.s.nunes74@gmail.com>
2023-06-17 10:25:41 -0300

    feat(dateService): retornando a data atual

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -1,7 +1,17 @@
 const getCurrentDate = () => {
-    // Add your solution here!
+	const atual = new Date();
+	const yyyy = atual.getFullYear();
+	let mm = atual.getMonth() + 1; // Months start at 0!
+	let dd = atual.getDate();
+
+	if (dd < 10) dd = '0' + dd;
+	if (mm < 10) mm = '0' + mm;
+
+	const formattedToday = yyyy + '-' + mm + '-' + dd;
 };
 
+return formattedToday
+
 module.exports = {
-    getCurrentDate
+	getCurrentDate
 }
